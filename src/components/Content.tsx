db40c589f24e2cb0a48e65684033606a12c3da9b
jorgefelipe <jorgefelipecunha@gmail.com>
2021-04-05 17:03:35 -0300

    Adicionando componentizaçao

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { MovieCard } from '../components/MovieCard';
+import { IGenreResponse, IMovie } from "../model/movies";
+
+interface ContentProps{
+  selectedGenre: IGenreResponse;
+  movies: IMovie[];
+}
+export function Content({movies, selectedGenre}: ContentProps ) {
+
+ return(
+     <div className="container">
+       <header>
+         <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+       </header>
+
+       <main>
+         <div className="movies-list">
+           {movies.map(movie => (
+             <MovieCard 
+               key ={movie.imdbID} 
+               title={movie.Title} 
+               poster={movie.Poster} 
+               runtime={movie.Runtime} 
+               rating={movie.Ratings[0].Value} 
+             />
+           ))}
+         </div>
+       </main>
+     </div>
+);
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { api } from "../services/api";
+import { Button } from '../components/Button';
+import '../styles/content.scss';
+import { IGenreResponse } from '../model/movies';
+
+
+interface SideBarProps{
+  onTransactionMovie: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({onTransactionMovie, selectedGenreId}: SideBarProps) {
+  const [genres, setGenres] = useState<IGenreResponse[]>([]);
+
+  useEffect(() => {
+    api.get<IGenreResponse[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+ return(  
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => onTransactionMovie(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+
+ );
 }
